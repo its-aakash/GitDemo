c9c57f6b6629b71b3405f68c2d04d127946a9696
akash <iamakashbhoyar@gmail.com>
2022-12-30 14:02:11 +0530

    this is commit from asian timezone guy

---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void postJira() {
+		System.out.println("postjira1");
+		System.out.println("postjira1");
+
+		System.out.println("postjira1");
+		System.out.println("postjira1");
+
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("Twitter deleted");
+	}
+
+}
